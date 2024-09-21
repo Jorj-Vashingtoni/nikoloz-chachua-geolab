5d65a1d2a997433d82ff1ad543e52a7c5b339945
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:53 +0400

    task №11

---
+++ nikoloz-chachua-geolab/homework №1/TASK11.cs
@@ -0,0 +1,32 @@
+﻿#if TASK11
+Console.WriteLine("to purchase the product we will need you to provide your location (country only)...");
+
+string country= Console.ReadLine();
+
+Console.WriteLine("now plese provide with your age...");
+
+int age = int.Parse(Console.ReadLine());
+
+if (age >= 0 && age <= 122) {
+    if (country == "America" && age >= 21 && age <= 122)
+    {
+        Console.WriteLine("you are allowed");
+    }
+    else if (country == "America" && age < 21)
+    {
+        Console.WriteLine("unfortunately you are not allowed yet");
+    }
+    else if (country != "America" && age >= 18)
+    {
+        Console.WriteLine("you are allowed");
+    }
+    else if (country != "America" && age < 18)
+    {
+        Console.WriteLine("unfortunately you are not allowed");
+    }
+}
+else
+{
+    Console.WriteLine("plaese provide valid data");
+}
+#endif
