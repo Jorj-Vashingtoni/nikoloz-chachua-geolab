e39e53b0326d9c6845fd25501d012fa0842d2edb
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:44 +0400

    task №9

---
+++ nikoloz-chachua-geolab/homework №1/TASK9.cs
@@ -0,0 +1,31 @@
+﻿#if TASK9
+Console.WriteLine("please enter your local time, firstly enter hours...");
+int hour = int.Parse(Console.ReadLine());
+Console.WriteLine("and now enter the minutes....");
+int minute = int.Parse(Console.ReadLine());
+
+
+if(hour >= 0 && hour <= 24 && minute >= 0 && minute <= 59)
+{
+    if (hour >= 5 && hour <= 11  )
+    {
+        Console.WriteLine("Good Morning");
+    }
+    else if (hour > 12 && hour <=16 )
+    {
+        Console.WriteLine("Good Afternoon");
+    }
+    else if (hour > 17 && hour <=20)
+    {
+        Console.WriteLine("Good Evening");
+    }
+    else 
+    {
+        Console.WriteLine("Good Night");
+    }
+}
+else
+{
+    Console.WriteLine("please provide valid data...");
+}
+#endif
