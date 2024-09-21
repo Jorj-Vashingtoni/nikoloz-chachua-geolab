17f305037b7100eb0c393678df428cf232ff13b3
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:19 +0400

    task №4

---
+++ nikoloz-chachua-geolab/homework №1/TASK4.cs
@@ -0,0 +1,18 @@
+﻿#if TASK4
+Console.WriteLine("Enter the year you doubt is not a leap year");
+int year = int.Parse(Console.ReadLine());
+
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+    {
+        Console.WriteLine($"the {year} is a leap year");
+    }
+    else if (year % 4 == 0 && year % 100 != 0)
+    {
+        Console.WriteLine($"the {year} is a leap year");
+    }
+    else
+    {
+        Console.WriteLine($"the {year} is not a leap year");
+    }
+
+    #endif
