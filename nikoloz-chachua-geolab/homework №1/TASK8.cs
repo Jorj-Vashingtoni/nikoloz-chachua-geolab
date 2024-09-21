a8bab7352b31a2c360ab568888ad58bdb68ed20d
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:39 +0400

    task №8

---
+++ nikoloz-chachua-geolab/homework №1/TASK8.cs
@@ -0,0 +1,21 @@
+﻿#if TASK8
+Console.WriteLine("plaese enter your age...");
+int age = int.Parse(Console.ReadLine());
+
+if (age >= 18  && age <= 122)
+{
+    Console.WriteLine("you are allowed to vote...");
+}
+else if (age < 0 )
+{
+    Console.WriteLine("please enter valid data...");
+}
+else if (age > 122)
+{
+    Console.WriteLine("please enter valid data...");
+}
+else
+{
+    Console.WriteLine("you are not allowed to vote yet");
+}
+#endif
