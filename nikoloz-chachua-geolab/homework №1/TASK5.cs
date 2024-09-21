f0a576501e3817d558a785931fecb377575dfb73
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:24 +0400

    task №5

---
+++ nikoloz-chachua-geolab/homework №1/TASK5.cs
@@ -0,0 +1,12 @@
+﻿#if TASK5
+Console.WriteLine("please to prove that you are not a robot provide with the password 'secret123'' ");
+string password = Console.ReadLine();
+
+if (password == "secret123")
+{
+    Console.WriteLine("access granted...");
+}
+else
+    Console.WriteLine("access denied...");
+
+    #endif
