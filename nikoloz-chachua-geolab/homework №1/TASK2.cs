b8db09b35827769f928e652938f6127848fea431
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:04 +0400

    task №2

---
+++ nikoloz-chachua-geolab/homework №1/TASK2.cs
@@ -0,0 +1,13 @@
+﻿#if TASK2
+Console.WriteLine("please provide with any number...");
+double num = double.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("the given number is even.");
+}
+else
+{
+    Console.WriteLine("the number is odd.");
+}
+#endif
