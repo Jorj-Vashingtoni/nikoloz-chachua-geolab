b38ed03cfd207307c201afed872ac9dcae61d227
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:16:33 +0400

    task №7

---
+++ nikoloz-chachua-geolab/homework №1/TASK7.cs
@@ -0,0 +1,30 @@
+﻿#if TASK7
+Console.WriteLine("please enter any operation of your choice ('+', '-', '*', '/', '%') ");
+string oper = Console.ReadLine();
+Console.WriteLine("okay now provide with two numbers...");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("now the second one...");
+double num2 = double.Parse(Console.ReadLine());
+
+
+if (oper == "+")
+{
+    Console.WriteLine("end product is: " + (num1+num2));
+}
+else if (oper == "-")
+{
+    Console.WriteLine("end product is: " + (num1 - num2));
+}
+else if (oper == "*")
+{
+    Console.WriteLine("end product is: " + (num1 * num2));
+}
+else if(oper == "/")
+{
+    Console.WriteLine("end product is: " + (num1 / num2));
+}
+else if (oper == "%")
+{
+    Console.WriteLine("end product is: " + (num1 % num2));
+}
+#endif
