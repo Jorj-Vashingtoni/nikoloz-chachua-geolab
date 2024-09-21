6a7dd63dfb609b3d5182c1fca89722762b704e85
Nikoloz Chachua <thenika600@gmail.com>
2024-09-21 17:15:58 +0400

    task №1

---
+++ nikoloz-chachua-geolab/homework №1/TASK1.cs
@@ -0,0 +1,17 @@
+﻿#if TASK1
+Console.WriteLine("please provide with an integer...");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 0)
+{
+    Console.WriteLine($"the given number --- {num} --- equals to 0");
+}
+else if (num > 0)
+{
+    Console.WriteLine($"the given number --- {num} --- is positive");
+}
+else if(num < 0)
+{
+    Console.WriteLine($"the given number --- {num} --- is negative");
+}
+#endif
