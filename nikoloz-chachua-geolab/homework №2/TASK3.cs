102647ca56c232e99e21a6fe2b18b6b3fae1abc2
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-01 18:57:45 +0400

    task №3

---
+++ nikoloz-chachua-geolab/homework №2/TASK3.cs
@@ -0,0 +1,40 @@
+﻿namespace nikoloz_chachua_geolab.homework__2;
+
+public class TASK3
+{
+    public static void Run()
+    {
+        Console.WriteLine("enter num under 10");
+
+        int num = int.Parse(Console.ReadLine());
+
+
+
+        do {
+
+            Console.WriteLine("please follow the instructions ");
+            num = int.Parse(Console.ReadLine());
+
+        }
+        while (num > 10);
+        Console.WriteLine($"the number is: {num}");
+        
+        int index = 0;
+
+        do {
+
+            Console.WriteLine("if you would like to continiue type yes...");
+            string confirmation = Console.ReadLine();
+            if (confirmation != "yes")
+            {
+                Console.WriteLine("you have chosen not to continiue....");
+                break;
+            }
+   
+
+
+            index++;
+        }
+        while (index != 3);
+    }
+}
