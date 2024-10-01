1fbc0aeea54a4e7a86e1031104cc3007f8d825e4
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-01 18:57:38 +0400

    task №2

---
+++ nikoloz-chachua-geolab/homework №2/TASK2.cs
@@ -0,0 +1,30 @@
+﻿namespace nikoloz_chachua_geolab.homework__2;
+
+public class TASK2
+{
+    public static void Run()
+    {
+        int i = 0;
+        int j = 1;
+        while (j <= 50)
+        {
+            i += j;
+            j++;
+        }
+        Console.WriteLine(i);
+        
+        int n = 0;
+        int sum = 0;
+        while (n < 5)
+        {
+            Console.WriteLine("enter 5");
+
+            int a=int.Parse(Console.ReadLine());
+
+            sum += a;
+
+            n++;
+        }
+        Console.WriteLine(sum);
+    }
+}
