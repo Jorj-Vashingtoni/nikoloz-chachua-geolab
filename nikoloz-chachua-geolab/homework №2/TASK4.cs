825f27e0c8a2d878f11e4b339f275f4b7bec20c7
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-01 18:57:51 +0400

    task №4

---
+++ nikoloz-chachua-geolab/homework №2/TASK4.cs
@@ -0,0 +1,16 @@
+﻿namespace nikoloz_chachua_geolab.homework__2;
+
+public class TASK4
+{
+    public static void Run()
+    {
+        for(int i = 1; i<=5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
