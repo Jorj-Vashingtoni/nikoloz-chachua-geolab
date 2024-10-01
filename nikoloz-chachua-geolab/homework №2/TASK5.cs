c1251f72ce5d7d8fbcf41f4ff7c37ac0d4e02c7a
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-01 18:57:57 +0400

    task №5

---
+++ nikoloz-chachua-geolab/homework №2/TASK5.cs
@@ -0,0 +1,25 @@
+﻿namespace nikoloz_chachua_geolab.homework__2;
+
+public class TASK5
+{
+    public static void Run()
+    {
+        for (int i = 3; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("fizz");
+            }
+            if (i % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("fizz buzz");
+            }
+            else
+                Console.WriteLine(i);
+        }
+    }
+}
