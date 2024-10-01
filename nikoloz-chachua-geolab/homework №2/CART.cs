b44a4a3125958f69156b1faa89856ea640f87c79
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-01 18:58:06 +0400

    cart

---
+++ nikoloz-chachua-geolab/homework №2/CART.cs
@@ -0,0 +1,24 @@
+﻿namespace nikoloz_chachua_geolab.homework__2;
+
+public class CART
+{
+    public static void Run()
+    {
+        double bal=0;
+        string confirmation;
+        do
+        {
+
+            Console.WriteLine("enter price of products...");
+            double price = double.Parse(Console.ReadLine());
+            bal += price;
+            Console.WriteLine("would you like to continiue?");
+            confirmation = Console.ReadLine();
+
+
+        }
+        while (confirmation == "yes");
+
+        Console.WriteLine($"the final price is:{bal} ");
+    }
+}
